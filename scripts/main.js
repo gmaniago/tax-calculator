31ed08dcff70563e644df7368734b551f69ec8e5
Gladys Maniago <gfmaniago@gmail.com>
2015-10-19 16:42:57 -0500

    adds breakpoint to results

--- scripts/main.js
+++ scripts/main.js
@@ -13,12 +13,12 @@ $submit.on('submit', function (e) {
 	var wisconsin = $state.val().toUpperCase();
 
 	var defaultSub = parseFloat($subtotal.val());
-	$result.html('Before Tax Amt: $' + defaultSub.toFixed(2) + ' State: ' + $state.val() + ' Total: $' + defaultSub.toFixed(2));
+	$result.html('Before Tax Amt: $' + defaultSub.toFixed(2) + '<br> State: ' + $state.val() + '<br> Total: $' + defaultSub.toFixed(2));
 	
 	if (wisconsin === 'WI' || wisconsin === 'WISCONSIN' || wisconsin === 'wisconsin' || wisconsin === 'Wi') {
-		defaultTax = 0.055;
+		defaultTax = 0.0550;
 		var totalTax = defaultSub * defaultTax;
 		var total = totalTax + defaultSub;
-		$result.html('Before Tax Amt: $' + defaultSub.toFixed(2) + ' State: ' + $state.val() + ' Subtotal: $' + defaultSub.toFixed(2) +' Tax Amount: $' + totalTax + ' Total: $' + parseFloat(Math.round(total * 100) / 100).toFixed(2));
+		$result.html('Before Tax Amt: $' + defaultSub.toFixed(2) + '<br> State: ' + $state.val() + '<br> Subtotal: $' + defaultSub.toFixed(2) +'<br> Tax Amount: $' + totalTax + '<br> Total: $' + parseFloat(Math.round(total * 100) / 100).toFixed(2));
 	}
 });
