16495442f2b31108871d966141bf0819ce2d359b
Gladys Maniago <gfmaniago@gmail.com>
2015-10-20 01:23:05 -0500

    added margin bottom to button

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var $ = require('jquery');
 var $submit = $('#submit');
 var $subtotal = $('#subtotal');
 var $state = $('#state');
-var $result = $('#result');
+var $calculation = $('#calculation');
 
 
 $submit.on('submit', function (e) {
@@ -13,12 +13,12 @@ $submit.on('submit', function (e) {
 	var wisconsin = $state.val().toUpperCase();
 
 	var defaultSub = parseFloat($subtotal.val());
-	$result.html('Before Tax Amt: $' + defaultSub.toFixed(2) + '<br> State: ' + $state.val() + '<br> Total: $' + defaultSub.toFixed(2));
+	$calculation.html('Pre-Tax Amt: $' + defaultSub.toFixed(2) + '<br> State: ' + $state.val() + '<br> Total: $' + defaultSub.toFixed(2));
 	
 	if (wisconsin === 'WI' || wisconsin === 'WISCONSIN' || wisconsin === 'wisconsin' || wisconsin === 'Wi') {
 		defaultTax = 0.0550;
 		var totalTax = defaultSub * defaultTax;
 		var total = totalTax + defaultSub;
-		$result.html('Before Tax Amt: $' + defaultSub.toFixed(2) + '<br> State: ' + $state.val() + '<br> Subtotal: $' + defaultSub.toFixed(2) +'<br> Tax Amount: $' + totalTax + '<br> Total: $' + parseFloat(Math.round(total * 100) / 100).toFixed(2));
+		$calculation.html('Before Tax Amt: $' + defaultSub.toFixed(2) + '<br> State: ' + $state.val() + '<br> Subtotal: $' + defaultSub.toFixed(2) +'<br> Tax Amount: $' + totalTax + '<br> Total: $' + parseFloat(Math.round(total * 100) / 100).toFixed(2));
 	}
 });
